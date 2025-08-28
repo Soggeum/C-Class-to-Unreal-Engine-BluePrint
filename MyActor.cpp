0b56d90451d09d5e3c009f2e32ab39a4ef36350e
Soggeum <joyten0@gmail.com>
2025-08-28 17:00:57 +0900

    udpate

---
+++ MyActor.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	start.X = 0;
+	start.Y = 0;
+	evCnt = 0;
+	totDist = 0;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Assignment5 Start!"));
+	UE_LOG(LogTemp, Warning, TEXT("Start Position: (%f, %f)"), start.X, start.Y);
+	move();
+	UE_LOG(LogTemp, Warning, TEXT("Total Move Distance: %d"), totDist);
+	UE_LOG(LogTemp, Warning, TEXT("Total Event Count: %d"), evCnt);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMyActor::move()
+{
+	for (int i = 0; i < 10; i++) {
+		FVector2D dest = start;
+		dest.X += step();
+		dest.Y += step();
+		float move_distance = distance(start, dest);
+
+		UE_LOG(LogTemp, Warning, TEXT("%dth position : (%f, %f) / move distance: %f"), i + 1, dest.X, dest.Y, move_distance);
+		start = dest;
+		totDist += move_distance;
+	}
+}
+
+int32 AMyActor::step()
+{
+	int32 RandomValue1 = FMath::RandRange(0, 1);
+	int32 RandomValue2 = FMath::RandRange(0, 1);
+	if (RandomValue2) {
+		evCnt++;
+		createEvent();
+	}
+
+	return RandomValue1;
+}
+
+float AMyActor::distance(FVector2D first, FVector2D second)
+{
+	float x = FMath::Square(first.X - second.X);
+	float y = FMath::Square(first.Y - second.Y);
+	
+	return FMath::Sqrt(x + y);
+}
+
+int32 AMyActor::createEvent()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Event Created!"));
+	return 0;
+}
---
+++ MyActor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Math/Vector2D.h"
+
+#include "MyActor.generated.h"
+
+UCLASS()
+class ASSIGNMENT5_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Actions")
+	void move();
+
+	UFUNCTION(BlueprintCallable, Category = "Actions")
+	int32 step();
+
+	float distance(FVector2D first, FVector2D second);
+	int32 createEvent();
+
+private:
+//	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyVariables")
+	FVector2D start;
+	int32 evCnt;
+	float totDist;
+};
